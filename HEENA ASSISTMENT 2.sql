70bcbd275a1e4336febaeb0bed987a961c96efc2
Hinamangnani <hinamangnani101@gmail.com>
2025-01-30 11:54:59 +0530

    Add files via upload

---
+++ HEENA ASSISTMENT 2.sql
@@ -0,0 +1,94 @@
+create schema student;
+
+create table student
+(
+StdId int not null,
+StdName varchar (25) not null unique,
+Sex varchar (25) not null,
+Percentage float,
+Class int,
+Sec varchar (2),
+Stream varchar (25) not null,
+DOB varchar (12) -- 3/8/1998, 3/08/1998
+);
+
+describe student;
+
+select * from student;
+
+select StdId, StdName, Sex, Percentage, Class, Sec, Stream, DOB from student;
+
+insert into student values (1001, 'Surekha Joshi', 'Female', 82, 12, 'A', 'Science', '3/8/1998');
+insert into student values (1002, 'MAAHI AGARWAL', 'Female', 56, 11, 'C', 'Commerce', '11/23/2008');
+insert into student values (1003, 'Sanam Verma', 'Male', 59, 11, 'C', 'Commerce', '6/29/2006');
+insert into student values (1004, 'Ronit Kumar', 'Male', 63, 11, 'C', 'Commerce', '11/5/1997');
+insert into student values (1005, 'Dipesh Pulkit', 'Male', 78, 11, 'B', 'Science', '14/9/2003');
+insert into student values (1006, 'JAHANVI Puri', 'Female', 60, 11, 'B', 'Commerce', '11/07/2008');
+insert into student values (1007, 'Sanam Kumar', 'Male', 23, 12, 'F', 'Commerce', '3/8/1998');
+insert into student values (1008, 'SAHIL SARAS', 'Male', 56, 11, 'C', 'Commerce', '11/7/2008');
+insert into student values (1009, 'AKSHRA AGARWAL', 'Female', 72, 12, 'B', 'Commerce', '10/1/1996');
+insert into student values (1010, 'STUTI MISHRA', 'Female', 39, 11, 'F', 'Science', '11/23/2008');
+insert into student values (1011, 'HARSH AGARWAL', 'Male', 42, 11, 'C', 'Commerce', '28/06/1998');
+insert into student values (1012, 'NIKUNJ AGARWAL', 'Male', 49, 12, 'C', 'Commerce', '3/8/1998');
+insert into student values (1013, 'AKRITI SAXENA', 'Female', 89, 12, 'A', 'Science', '11/23/2008');
+insert into student values (1014, 'TANI RASTOGI', 'Female', 82, 12, 'A', 'Science', '11/23/2008');
+
+describe student;
+
+select * from student;
+
+drop table student;
+
+create table student
+(
+StdId int not null unique,
+StdName varchar (25) not null unique,
+Sex varchar (25) not null,
+Percentage float,
+Class int,
+Sec varchar (2),
+Stream varchar (25) not null,
+DOB varchar (25) -- 3/8/1998, 3/08/1998
+);
+
+describe student;
+
+select * from student;
+
+select StdId, StdName, Sex, Percentage, Class, Sec, Stream, DOB from student;
+
+select * from student;
+
+insert into student values (1001, 'Surekha Joshi', 'Female', 82, 12, 'A', 'Science', '3/8/1998');
+insert into student values (1002, 'MAAHI AGARWAL', 'Female', 56, 11, 'C', 'Commerce', '11/23/2008');
+insert into student values (1003, 'Sanam Verma', 'Male', 59, 11, 'C', 'Commerce', '6/29/2006');
+insert into student values (1004, 'Ronit Kumar', 'Male', 63, 11, 'C', 'Commerce', '11/5/1997');
+insert into student values (1005, 'Dipesh Pulkit', 'Male', 78, 11, 'B', 'Science', '14/9/2003');
+insert into student values (1006, 'JAHANVI Puri', 'Female', 60, 11, 'B', 'Commerce', '11/07/2008');
+insert into student values (1007, 'Sanam Kumar', 'Male', 23, 12, 'F', 'Commerce', '3/8/1998');
+insert into student values (1008, 'SAHIL SARAS', 'Male', 56, 11, 'C', 'Commerce', '11/7/2008');
+insert into student values (1009, 'AKSHRA AGARWAL', 'Female', 72, 12, 'B', 'Commerce', '10/1/1996');
+insert into student values (1010, 'STUTI MISHRA', 'Female', 39, 11, 'F', 'Science', '11/23/2008');
+insert into student values (1011, 'HARSH AGARWAL', 'Male', 42, 11, 'C', 'Commerce', '28/06/1998');
+insert into student values (1012, 'NIKUNJ AGARWAL', 'Male', 49, 12, 'C', 'Commerce', '3/8/1998');
+insert into student values (1013, 'AKRITI SAXENA', 'Female', 89, 12, 'A', 'Science', '11/23/2008');
+insert into student values (1014, 'TANI RASTOGI', 'Female', 82, 12, 'A', 'Science', '11/23/2008');
+
+describe student;
+
+select * from student;
+
+describe student;
+
+
+
+select * from student;
+
+
+
+
+
+
+
+
+
---
+++ HEENA ASSISTMENT..sql
@@ -0,0 +1,102 @@
+create schema worker;
+
+create table employees2
+(
+worker_id int not null unique,
+first_name varchar (25) not null,
+last_name varchar (25) not null,
+salary int not null,
+joining_date varchar (25) not null, -- 2/20/2024 9.00
+department varchar (25) not null
+);
+
+select * from employees2;
+
+insert into employees2 values (1 ,'Monika' , 'Arora' , 100000 , '2/20/2024 9:00' , 'HR');
+insert into employees2 values (2 ,'Niharika' , 'Verma' , 80000 , '6/11/2014 9:00' , 'Admin');
+insert into employees2 values (3 ,'Vishal' , 'Singhal' , 300000 , '2/20/2024 9:00', 'HR');
+insert into employees2 values (4 ,'Amitabh' , 'Singh' , 500000 , '2/20/2024 9:00' ,'Admin');
+insert into employees2 values (5 ,'Vivek' , 'Bhati' , 50000 , '6/11/2014 9:00' , 'Admin');
+insert into employees2 values (6 ,'Vipul' , 'Diwan' , 200000 , '6/11/2014 9:00' ,'Account');
+insert into employees2 values (7 ,'Satish' , 'Kumar' , 75000 , '1/20/2014 9:00','Account');
+insert into employees2 values (8 ,'Geetika' , 'Chauhan' , 90000 , '4/11/2014 9:00','Admin');
+
+
+select * from employees2;
+
+describe employees2;
+
+drop table employees2;
+
+create table employees2
+(
+worker_id int not null unique,
+first_name varchar (25) not null,
+last_name varchar (25) not null,
+salary int not null,
+joining_date varchar (25) not null, -- 2/20/2024 9.00
+department varchar (25) not null
+);
+
+select worker_id,first_name,last_name.salary,joining_date,department from employees2;
+
+select * from employees2;
+
+insert into employees2 values (1 ,'Monika' , 'Arora' , 100000 , '2/20/2024 9:00' , 'HR');
+insert into employees2 values (2 ,'Niharika' , 'Verma' , 80000 , '6/11/2014 9:00' , 'Admin');
+insert into employees2 values (3 ,'Vishal' , 'Singhal' , 300000 , '2/20/2024 9:00', 'HR');
+insert into employees2 values (4 ,'Amitabh' , 'Singh' , 500000 , '2/20/2024 9:00' ,'Admin');
+insert into employees2 values (5 ,'Vivek' , 'Bhati' , 50000 , '6/11/2014 9:00' , 'Admin');
+insert into employees2 values (6 ,'Vipul' , 'Diwan' , 200000 , '6/11/2014 9:00' ,'Account');
+insert into employees2 values (7 ,'Satish' , 'Kumar' , 75000 , '1/20/2014 9:00','Account');
+insert into employees2 values (8 ,'Geetika' , 'Chauhan' , 90000 , '4/11/2014 9:00','Admin');
+
+update employees2 set first_name = 'Geetika' where worker_id = 8;
+
+
+select * from employees2;
+
+
+
+Select * from employees2 order by (first_name) asc , (department) desc;
+
+select * from emplopyees2 where first_name in ('Vipul', 'Satish');
+
+select * from employees2 where first_name like '%h';
+
+select * from employees2 where salary>=50000 and salary<=50000;
+
+
+
+select * from employees2 limit 6;
+
+
+select department, count(*) as number_people from employees2 order by department;
+
+select department,first_name, max(salary) from employees2 group by department, first_name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
